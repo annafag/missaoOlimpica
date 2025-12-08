ac4d572ce362c7620ae89931c17a3cafdcc584dd
annafag <fernandes.oliveira.anna@escola.pr.gov.br>
2025-12-08 09:14:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const modalidades = ["Ginástica", "Judô", "Surfe", "Vôlei"];
 
 //Com base na constante modalidade, 
 //Coloque o número que represente o esporte do seu grupo
-const escolha = ?;
+const escolha = 3;
 
 document.querySelector('body').style.backgroundImage = "url('img/"+modalidades[escolha]+".png')";
 document.querySelector('title').textContent = "Missão Olímpica | "+modalidades[escolha];
@@ -20,7 +20,7 @@ let historiaFinal = "";
 
 //Assim como a variável atual (acima)
 //Crie uma variável com o nome pontos que inicie com 0
-
+let pontos= 0
 
 function mostraPergunta(){
     if(atual >= perguntas[escolha].length){
@@ -56,28 +56,28 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Resultado";
     caixaAlternativas.textContent = "";
     // chame a função podiumMedalhas aqui
-    
+    podiumMedalhas
+
 }
 
-//crie uma função podiumMedalhas
-//E verifique a quantidade de pontos
-//  - Se pontos igual a 3 ganha bronze, dentro dessa condicional:
-//    Utilize a linha de código abaixo para modificar o CSS e inserir uma medalha
-//    caixaPrincipal.style.backgroundImage = "url('img/bronze.png')";
-//    altere o textContent do caixaPerguntas para "Resultado da competição: 3 pontos é BRONZE!"
-//  - Se pontos igual a 4 ganha prata
-//    Utilize a linha de código abaixo para modificar o CSS e inserir uma medalha
-//    caixaPrincipal.style.backgroundImage = "url('img/prata.png')";
-//    altere o textContent do caixaPerguntas para "Resultado da competição: 4 pontos é PRATA!"
-//  - Se pontos igual a 5 ganha ouro
-//    Utilize a linha de código abaixo para modificar o CSS e inserir uma medalha
-//    caixaPrincipal.style.backgroundImage = "url('img/ouro.png')";
-//    altere o textContent do caixaPerguntas para "Resultado da competição: 5 pontos é OURO!"
-//  - Se pontos menor que 3
-//    Utilize a linha de código abaixo para modificar o CSS e inserir uma medalha
-//    caixaPrincipal.style.backgroundImage = "url('img/perdeu.png')";
-//    altere o textContent do caixaPerguntas para "Resultado da competição: PERDEU!";
-//2 Dentro de cada SE altere o conteúdo de texto de caixaPerguntas exemplo
-//     Quando ouro coloque "Resultado da competição: 3 pontos é BRONZE!";
 
-mostraPergunta(); 
+function podiumMedalhas() {
+
+  if (pontos === 3) {
+    caixaPrincipal.style.backgroundImage = "url('img/bronze.png')";
+    caixaPerguntas.textContent = "Resultado da competição: 3 pontos é BRONZE!";
+
+  } else if (pontos === 4) {
+    caixaPrincipal.style.backgroundImage = "url('img/prata.png')";
+    caixaPerguntas.textContent = "Resultado da competição: 4 pontos é PRATA!";
+
+  } else if (pontos === 5) {
+    caixaPrincipal.style.backgroundImage = "url('img/ouro.png')";
+    caixaPerguntas.textContent = "Resultado da competição: 5 pontos é OURO!";
+
+  } else if (pontos < 3) {
+    caixaPrincipal.style.backgroundImage = "url('img/perdeu.png')";
+    caixaPerguntas.textContent = "Resultado da competição: PERDEU!";
+  }
+}
+mostraResultado
